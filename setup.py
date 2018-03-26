200871d735b91739dee8db6f9a01e45a40711033
AntoinePrv <antoine.prouvost.2013_dev@polytechnique.org>
2018-03-26 19:29:36 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Caprices",
+    author="Antoine Prouvost"
+    version="0.1",
+    packages=find_packages(),
+    license="MIT",
+	long_description=open("README.md").read(),
+)
