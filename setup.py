9479f8a30b482729ab03be2d103af1cd66e3abca
AntoinePrv <antoine.prouvost.2013_dev@polytechnique.org>
2018-03-26 19:32:06 -0400

    Shamefully correct untested code

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Caprices",
-    author="Antoine Prouvost"
+    author="Antoine Prouvost",
     version="0.1",
     packages=find_packages(),
     license="MIT",
